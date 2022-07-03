0ae2acefff1ebbe53095c3c21fec36df35608af0
Andrii Salogub <andrejsalogub3@gmail.com>
2022-07-03 16:14:07 +0300

    add functionality to change screen from start game to choose time

---
+++ app.js
@@ -0,0 +1,7 @@
+const startBtn = document.querySelector("#start");
+const screens = document.querySelectorAll(".screen");
+
+startBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    screens[0].classList.add("up");
+});
